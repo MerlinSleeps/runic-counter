3247ceaebd0fb31c871dba7150679f71a7e3f6e6
MerlinSleeps <44499558+MerlinSleeps@users.noreply.github.com>
2025-11-14 20:41:18 +0100

    added PWA

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,34 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
   base: '/runic-counter/', 
-  plugins: [react()],
+  plugins: [react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['icons/runes/*.jpg'],
+      manifest: {
+        name: 'Runic Counter',
+        short_name: 'Runic Counter',
+        start_url: '/runic-counter/',
+        display: 'standalone',
+        background_color: '#0A1428',
+        theme_color: '#0A1428',
+        orientation: 'portrait',
+        icons: [
+          {
+            src: 'icons/icon-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'icons/icon-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+    })
+  ],
 })
